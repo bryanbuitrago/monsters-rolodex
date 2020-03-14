66bd6ef0311d08ef7ac08ccc44584ff0b7734f61
Bryan <bryanbuitrago86@gmail.com>
2020-03-13 20:17:16 -0400

    Updates

---
+++ precache-manifest.4385e0d264f209e7f100b3b075ced25f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df34f6f0ed4eb2afcc9e8e08c12e6bf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "44f7af58346e045df394",
+    "url": "/monsters-rolodex/static/css/main.eb43bc74.chunk.css"
+  },
+  {
+    "revision": "7fe0845a347327439809",
+    "url": "/monsters-rolodex/static/js/2.80027728.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.80027728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44f7af58346e045df394",
+    "url": "/monsters-rolodex/static/js/main.5fde3a84.chunk.js"
+  },
+  {
+    "revision": "0a7538a84e8ef0422b17",
+    "url": "/monsters-rolodex/static/js/runtime-main.984b980e.js"
+  }
+]);
